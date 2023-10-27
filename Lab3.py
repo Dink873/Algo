049783152cc5f618e1ac91c11e0e9e6f007b2a97
Dink873 <119216594+Dink873@users.noreply.github.com>
2023-10-27 17:45:52 +0300

    Update Lab3.py

--- Lab3.py
+++ Lab3.py
@@ -1,27 +1,52 @@
+from collections import deque
+
+
 class BinaryTree:
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
 
-def branchSums(root):
-    def calculate_branch_sums(node, current_sum):
-        if node is None:
-            return 0
+def sum_left_leaves(root):
+    if root is None:
+        return 0
 
-        current_sum += node.value
+    left_sum = 0
 
-        # Ïåðåâ³ðÿºìî, ÷è âóçîë º ë³âèì ëèñòêîì
-        if node.left is None and node.right is None:
-            return current_sum
+    queue = deque()
 
-        left_sum = calculate_branch_sums(node.left, current_sum)
-        right_sum = calculate_branch_sums(node.right, current_sum)
+    queue.append(root)
 
-        return left_sum + right_sum
+    while queue:
+        node = queue.popleft()
+ 
+        if node.left and not node.left.left and not node.left.right:
+            left_sum += node.left.value
 
-    return calculate_branch_sums(root, 0)
+        if node.left:
+            queue.append(node.left)
 
+        if node.right:
+            queue.append(node.right)
+########### стек ################
+    #stack = [root]
+
+    #while stack:
+    #    node = stack.pop()
+     #   if node.left and not node.left.left and not node.left.right:
+      #      left_sum += node.left.value
+       # if node.left:
+        #    stack.append(node.left)
+        #if node.right:
+         #   stack.append(node.right)
+##########  рекурсивно ###############
+    #if root.left and not root.left.left and not root.left.right:
+    #    left_sum += root.left.value
+
+    #left_sum += sum_left_leaves(root.left)
+    #left_sum += sum_left_leaves(root.right)
+
+    return left_sum
 
 root = BinaryTree(3)
 root.left = BinaryTree(9)
@@ -29,5 +54,5 @@ root.right = BinaryTree(20)
 root.right.left = BinaryTree(15)
 root.right.right = BinaryTree(7)
 
-result = branchSums(root)
+result = sum_left_leaves(root)
 print(result)
